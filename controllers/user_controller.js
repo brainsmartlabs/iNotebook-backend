c52779e4d00deb1d4422a1ed824ce27da181b20c
Balaji The Boss <brainsmartlabs@gmail.com>
2023-03-14 23:21:22 +0530

    checking the preexisting user before signup

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -5,6 +5,17 @@ const User = require('../models/User');
 module.exports.signUp = async (req, res) => {
     let user;
     try {
+        user = await User.findOne({ email: req.body.email });
+    } catch (e) {
+        return res.status(400).json({ errorMsg: "Some Error at the database Occured", e });
+    }
+
+
+    if (user) {
+        return res.status(400).json({ error: "Sorry, user Already Exisits with the email" });
+    }
+
+    try {
         user = new User(req.body);
         await user.save();
         res.status(200).json(user);
