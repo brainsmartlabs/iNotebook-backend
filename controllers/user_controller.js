2b2c39d4ec6aa87961acee56621bc9ad4062a6cc
Balaji The Boss <brainsmartlabs@gmail.com>
2023-03-14 16:36:10 +0530

    made first DB transaction

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,12 +1,16 @@
 const mongoose = require('mongoose');
+const User = require('../models/User');
 
 module.exports.getAllUsers = (req, res) => {
-    
+
 }
 
-module.exports.signUp = (req, res) => {
+module.exports.signUp = async (req, res) => {
     console.log(req.body);
-    res.send(req.body);
+    //let {name, email, password} = req.body;
+    let user = new User(req.body);
+    await user.save();
+    res.json(user);
 }
 
 module.exports.loginUser = (req, res) => {
